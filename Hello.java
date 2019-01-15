9c6f80c4bdfadc13a4a82d6dd961ed3bfc6040f8
fiazd <46724673+fiazd@users.noreply.github.com>
2019-01-15 14:01:56 -0500

    Add files via upload
    Daanish Fiaz 1/15/18 Lab 1 CMSC 255

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/***********************************************
+ * Java Program Name
+ ***********************************************
+ *Project/Program description
+ *______________________________________________
+ *Student First Name Last Name
+ *Date
+ *Course Number and Section
+ ***********************************************/
+ 
+ public class Hello {
+  public static void printHeading() {
+   System.out.println("Daanish Fiaz");
+   System.out.println("Lab 1");
+   System.out.println("Getting a program to print");
+   System.out.println("January 15th, 2018");
+   System.out.println("CMSC 255 02");
+   System.out.println();
+  }
+  
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
